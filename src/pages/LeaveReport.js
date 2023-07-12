8183a0fbf118c9d8ad12d4f5e88ef536d51e9f37
Manoj Jadhav <manojjadhav1097@gmail.com>
2023-07-12 13:16:31 +0530

    fix leave report problem

--- src/pages/LeaveReport.js
+++ src/pages/LeaveReport.js
@@ -3,19 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import StudentLeaveReport from "../components/StudentLeaveReport";
 import TeacherLeaveReport from "../components/TeacherLeaveReport";
 import { getLeaveRequest } from "../redux/actions";
+import Loader from "../components/Loader";
 
 const LeaveReport = () => {
   const dispatch = useDispatch();
-  const { userdata } = useSelector((state) => state.userDetail);
-  console.log(userdata)
+  const { userdata, loading } = useSelector((state) => state.userDetail);
+  console.log(userdata);
   useLayoutEffect(() => {
     dispatch(getLeaveRequest());
-    
   }, []);
-  
+
   return (
     <>
-      {userdata.userType === "S" ? (
+      {loading ? (
+        <Loader />
+      ) : userdata.userType === "S" ? (
         <StudentLeaveReport />
       ) : (
         <TeacherLeaveReport />
